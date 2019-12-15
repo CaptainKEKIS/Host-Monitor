980585078909db6316725e0ccb19e2ef22885456
IFOP0112003 Кустов Дмитрий Вячеславович <IFOP0112003@sibupk.su>
2019-12-15 22:00:09 +0700

    123

--- AdminPanel/MainWindow.xaml.cs
+++ AdminPanel/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Net.Http;
 using System.Net.NetworkInformation;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -28,29 +29,35 @@ namespace AdminPanel
         public MainWindow()
         {
             InitializeComponent();
+
             List<Host> hosts = new List<Host>();
-
-            string address = "http://localhost:10500/api/hosts";
-            try
-            {
-                using (var client = new HttpClient())
-                {
-                    HttpResponseMessage Response = client.GetAsync(address).Result;
-                    if (Response.StatusCode == HttpStatusCode.OK)
-                    {
-                        HttpContent responseContent = Response.Content;
-                        var json = responseContent.ReadAsStringAsync().Result;
-                        hosts = JsonConvert.DeserializeObject<List<Host>>(json);
-                    }
-                }
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-
             ResultDataGrid.ItemsSource = hosts;
+            string address = "http://localhost:10500/api/hosts";
+            Task task = new Task(() =>
+                {
+                    try
+                    {
+                        using (var client = new HttpClient())
+                        {
+                            HttpResponseMessage Response = client.GetAsync(address).Result;
+                            if (Response.StatusCode == HttpStatusCode.OK)
+                            {
+                                HttpContent responseContent = Response.Content;
+                                var json = responseContent.ReadAsStringAsync().Result;
+                                hosts = JsonConvert.DeserializeObject<List<Host>>(json);
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Произошла ошибка!\n" + ex.Message);
+                    }
+
+                    Thread.Sleep(10000);
+                }
+            );
+            task.Start();
+            ResultDataGrid.Items.Refresh();
         }
     }
 }
-
